c8cc60864b28e4702c43b3f04c7357d810928290
Jmart5564 <Jmart5564@gmail.com>
2022-08-09 17:45:55 -0700

    sign in test passing

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,12 +2,22 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const fakeUser = {
   firstName: 'Momo',
   lastName: 'Stu',
-  email: 'Momo@stu.com',
+  email: 'Momo@defense.gov',
   password: 'meow'
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? fakeUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...fakeUser, userProps });
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
 };
 
 describe('authentication and authorization routes', () => {
@@ -27,11 +37,10 @@ describe('authentication and authorization routes', () => {
     });
   });
 
-  it('POST /api/v1/users/sessions should log a user in', async () => {
+  it('POST should sign in an existing user', async () => {
     await request(app).post('/api/v1/users').send(fakeUser);
-    const res = await request(app).post('/api/v1/users/session').send(fakeUser);
+    const res = await request(app).post('/api/v1/users/sessions').send({ email: 'Momo@defense.gov', password: 'meow' });
     expect(res.status).toBe(200);
-    expect(res.body.message).toEqual('Signed in successfully!');
   });
   afterAll(() => {
     pool.end();
